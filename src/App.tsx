d95ddcf84ccdc531d1296b7e110e763b9b4c9692
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-12-22 05:08:50 +0900

    #7.2 Drag and Drop part One
    ## 목표
    
    - 드래그 앤 드롭을 구현해 본다.
    - recoil로 데이터를 모델링 하는 법을 배워본다.
    
    ## `react-beautiful-dnd`
    
    [react-beautiful-dnd](https://www.npmjs.com/package/react-beautiful-dnd)
    
    - 유명하고 유용한 드래그앤 드롭 라이브러리
    
    ### 사용법
    
    1. `npm i react-beautiful-dnd` 설치
        - 타입 스크립트 환경의 경우 : `@types/` 버전 설치
    2. `react-beautiful-dnd` 의 기본 구조
    
        ```tsx
        <DragDropContext>
        	[<](https://github.com/atlassian/react-beautiful-dnd/blob/HEAD/docs/api/droppable.md)Droppable>
        		<Draggable>
        		</Draggable>
        	[<](https://github.com/atlassian/react-beautiful-dnd/blob/HEAD/docs/api/droppable.md)/Droppable>
        </DragDropContext>
        ```
    
        - `[**<DragDropContext onDragEnd={}>](https://github.com/atlassian/react-beautiful-dnd/blob/HEAD/docs/api/drag-drop-context.md)` : 드래그 앤 드롭 UI를 구현 할 영역 전체를 감싼다.**
            - `onDragEnd` :
                - `<DragDropContext />` 의 필수 Props
                - 드래그가 끝난 시점에 불려지는 함수
            - Children Element 를 필수로 갖어야 한다.
        - `[**<Droppable draggableId="">](https://github.com/atlassian/react-beautiful-dnd/blob/HEAD/docs/api/droppable.md)` : 드롭 할 수 있는 영역**
            - `draggableId` :
                - [`<](https://github.com/atlassian/react-beautiful-dnd/blob/HEAD/docs/api/droppable.md)Droppable />` 의 필수 Props
                - 드롭 영역이 여러개 일 수 있기 때문에 고유 id를 부여 해주어야 한다.
            - [`<](https://github.com/atlassian/react-beautiful-dnd/blob/HEAD/docs/api/droppable.md)Droppable />` Children은 React Element이면 안된다. 함수 여야 한다.
        - `[**<Draggable draggableId='' index />](https://github.com/atlassian/react-beautiful-dnd/blob/HEAD/docs/api/draggable.md)` : 드래그 할 수 있는 요소**
            - [`<](https://github.com/atlassian/react-beautiful-dnd/blob/HEAD/docs/api/droppable.md)Draggable />` 은 두 가지의 필수 Props가 필요 하다.
                - `draggableId`
                - `index`
            - `<Draggable>`  Children은 React Element이면 안된다. 함수 여야 한다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import PracticeSelector from './Components/PracticeSelector';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function App() {
+  const onDragEnd = () => {};
+
   return (
     <>
-      <h2>#7.1 Set Selectors</h2>
-      <PracticeSelector />
+      <div>
+        <h2>#7.1 Set Selectors</h2>
+        <PracticeSelector />
+      </div>
+      <div>
+        <h2>Drag and Drop</h2>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="one">
+            {() => (
+              <ul>
+                <Draggable draggableId="one" index={0}>
+                  {() => <li>One</li>}
+                </Draggable>
+                <Draggable draggableId="two" index={1}>
+                  {() => <li>Two</li>}
+                </Draggable>
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
     </>
   );
 }
