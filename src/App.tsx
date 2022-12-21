bd6fdfd4b217978c3dff88ff12eee7e90dbeb2cf
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-12-22 04:34:21 +0900

    #7.1 Set Selectors 실습 및 폴더 분류
    ## 목표
    
    - `selectors` Hook의 `set()` 함수에 대해서 배워본다.
    
    ## `selectors` Hook의 `set()` 함수
    
    [selector(options) | Recoil](https://recoiljs.org/ko/docs/api-reference/core/selector/)
    
    - `selectors` Hook의 `get()` 함수가 기존의 State를 가공하여 새로운 값을 return 하는 것이라면,
    - `selectors` Hook의 `set()` 함수는 기존의 State 자체를 수정 할 수 있다.
    
    ### 사용법
    
    - `[useRecoilState()](https://www.notion.so/5e1149e6812643cd9a00f3de10434f59)` 를 사용해서 `selectors` 에 접근하면, 컴포넌트에서 `get()`이 return 하는 값과 `set()` 함수를 사용 할 수 있다.
        - `src/atoms.tsx`
    
            ```tsx
            import {selector } from "recoil";
    
            export const selectorName = selector<number>({
                key: 'hours',
                get: ({get}) => {
                  const minutes = get(minuteState);
                  return minutes/60;
                },
            		set: ({ set }, newValue) => {
            	    const minutes = Number(newValue) * 60;
            	    set(minuteState, minutes);
            	  },
            });
            ```
    
            - `set` Property의 두 번째 argument (`newValue`)
                - 컴포넌트에서 `set()` 함수를 실행 할 때, 인수로 받아 올 값
            - `set` 함수의 argument
                - 첫 번째 : 수정 할 atom State
                - 두 번째 : atom State에 입력 할 값
            - 타입스크립트 환경이라면, selector의 반환 값에 `<number>` 형식으로 타입을 정해줄 수 있다.
                - `atom`은 `default` 값이 명시되어 있지만, `selector` 는 어떤 값을 input으로 받을 지 모르기 때문..
                    - 2022년 12월 22일 현재 버전은 `selector` 도 타입스크립트가 입력값의 타입을 인지 한다..
        - 컴포넌트
    
            ```tsx
            import { useRecoilState } from 'recoil';
            import { selectorName } from './atoms';
    
            const [value, setValue] = useRecoilState(selectorName);
    
            setValue(newValue);
            ```
    
            - 그냥 `atom` 상태를 가져오는 것과 똑같이 생겼지만, `selector`는 State를 가공 할 수 있다.
            - `useRecoilState(selector)` 가 반환 하는 date
                - 첫 번째 argument :
                    - `selector` 의 `get()` 함수가 `return` 하는 값
                - 두 번째 argument :
                    - `selector` 의 `set()` Property를 실행 시키는 함수

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import React from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { hourSelector, minuteState } from './atoms';
+import PracticeSelector from './Components/PracticeSelector';
 
 function App() {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
-  const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value);
-  };
-
   return (
     <>
-      <input
-        value={minutes}
-        onChange={onMinuteChange}
-        type="number"
-        placeholder="Minutes"
-      />
-      <input value={hours} type="number" placeholder="Hours" />
+      <h2>#7.1 Set Selectors</h2>
+      <PracticeSelector />
     </>
   );
 }
---
+++ src/Components/PracticeSelector.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useRecoilState } from 'recoil';
+import { hourSelector, minuteState } from '../atoms';
+
+function PracticeSelector() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+  const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+  const onHourChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  };
+
+  return (
+    <>
+      <input
+        value={minutes}
+        onChange={onMinuteChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input
+        value={hours}
+        onChange={onHourChange}
+        type="number"
+        placeholder="Hours"
+      />
+    </>
+  );
+}
+
+export default PracticeSelector;
