4d0c5a508ad216878aea423cb9b94047cf1118db
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-12-22 03:46:00 +0900

    #7 TRELLO CLONE 실습 파일 초기 설정 및 #7.0 Get Selectors
    ## 개요
    
    - 이번 섹션에서는 [Kanban](https://www.google.com/search?q=Kanban&newwindow=1&sxsrf=ALiCzsY3KYAaSBJAKmZNpNcVV0tfP1Lqlw:1671640386705&source=lnms&tbm=isch&sa=X&ved=2ahUKEwimgMTbkYv8AhUBWHwKHTusB1kQ_AUoAXoECAEQAw&biw=1920&bih=937&dpr=1) 어플리케이션을 만들어 본다.
        - To do list 인데, 목록별로 나뉘어져 있다.
    - 이를 통해 **드래그 앤 드롭**을 학습해 본다.
        - Recoil 등을 사용해서 데이터를 어떻게 모델링 하고 구성할지 연습 해 볼 수 있다.
    - 그 전에 먼저 [recoil 의 `selectors` Hook](https://www.notion.so/06-STATE-MANAGEMENT-5e1149e6812643cd9a00f3de10434f59), 그 중에서도 `selectors` Hook 의 `**set` 함수**에 대해서 배워보기 위하여, 작은 프로젝트를 진행 해 본다.
    
    ## ****Get Selectors 의 이해와 Set Selectors의 필요****
    
    ```tsx
    export const minuteState = atom({
        key: 'minutes',
        default: 0
    });
    
    export const hourSelector = selector({
        key: 'hours',
        get: ({get}) => {
            const minutes = get(minuteState);
            return minutes/60;
        }
    });
    ```
    
    ### `selectors` Hook의 `get()` 함수
    
    - State를 input으로 받아와서 수정 후 output으로 return 할 수 있다.
    
    ### `selectors` Hook의 `set()` 함수가 필요 한 이유
    
    - `get()` 함수는 atom 내에서 state를 받아와서 수정 할 수 있을 뿐, 컴포넌트에서 직접 값을 입력 받을 수 없다.
    
    ## 참고
    
    ### 쉽게 type 변환을 할 수 있는 자바스크립트 문법
    
    - `number` → `string` :
        - `1 + ""` ⇒ `"1"`
        - number 타입의 숫자에 따옴표(`""` or `''`)를 더해준다.
    - `string` → `number`  :
        - `"1" +` ⇒ `1`
        - 문자 타입의 숫자에 플러스 기호(`+`)를 붙여준다.

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  bgColor: '#222f3e',
+  textColor: '#c8d6e5',
+  boxBgColor: '#ffffff',
+  accentColor: '#1dd1a1',
+};
