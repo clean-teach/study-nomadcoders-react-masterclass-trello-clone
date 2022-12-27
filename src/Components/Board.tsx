4dca4ce666095f7e9a58be2f9bd9dd62ac471613
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-12-27 23:04:07 +0900

    #7.14 Task Objects
    ## 목표
    
    - 할 일 목록에 할 일을 추가하는 form을 구현 해본다.
    
    ## 구현
    
    1. `[react-hook-form](https://www.notion.so/06-STATE-MANAGEMENT-5e1149e6812643cd9a00f3de10434f59)` 을 사용해서 form 기능을 구현한다.
    2. atom 에서 관리하는 state의 구조를 **string으로 이루어진 배열**에서 **object로 이루어진 배열**로 바꿀 것이다.
    3. `TypeScript`  환경이라면, interface 등을 사용해서 data의 type을 정의 해준다.
    4. Type 에 맞게 각 컴포넌트의 Props로 필요한 데이터를 전달 해 준다.
    
    ## 정리
    
    - Form 기능을 구현하고, State의 형태를 수정 하였으나
    - To do List를 추가 하거나, 드래그앤 드롭으로 옮길 때 atom State에 반영하는 작업이 필요 하다.

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -1,7 +1,8 @@
-import React, { useRef } from 'react';
+import { useForm } from 'react-hook-form';
 import { Droppable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 import DragabbleCard from './DragabbleCard';
+import { ITodo } from '../atoms';
 
 const Wrapper = styled.div`
   width: 300px;
@@ -38,25 +39,41 @@ const Area = styled.div<IAreaProps>`
   padding: 20px;
 `;
 
+const Form = styled.form`
+  width: 100%;
+  input {
+    width: 100%;
+  }
+`;
+
 interface IBoardProps {
-  toDos: string[];
+  toDos: ITodo[];
   boardId: string;
 }
 
+interface IForm {
+  toDo: string;
+}
+
 function Board({ toDos, boardId }: IBoardProps) {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const onClick = () => {
-    inputRef.current?.focus();
-    setTimeout(() => {
-      inputRef.current?.blur();
-    }, 5000);
+  const { register, setValue, handleSubmit } = useForm<IForm>();
+  const onValid = (info: IForm) => {
+    console.log(info);
+    // const onValid = ({ toDo }: IForm) => {
+    //   console.log(toDo);
+    setValue('toDo', '');
   };
 
   return (
     <Wrapper>
       <Title>{boardId}</Title>
-      <input ref={inputRef} placeholder="grab me" />
-      <button onClick={onClick}>click me</button>
+      <Form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register('toDo', { required: true })}
+          type="text"
+          placeholder={`Add task on ${boardId}`}
+        />
+      </Form>
       <Droppable droppableId={boardId}>
         {(magic, info) => (
           <Area
@@ -66,7 +83,12 @@ function Board({ toDos, boardId }: IBoardProps) {
             {...magic.droppableProps}
           >
             {toDos.map((toDo, index) => (
-              <DragabbleCard key={toDo} index={index} toDo={toDo} />
+              <DragabbleCard
+                key={toDo.id}
+                index={index}
+                toDoId={toDo.id}
+                toDoText={toDo.text}
+              />
             ))}
             {magic.placeholder}
           </Area>
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -17,16 +17,20 @@ export const hourSelector = selector({
   },
 });
 
+export interface ITodo {
+  id: number;
+  text: string;
+}
 interface IToDoState {
-  [key: string]: string[];
+  [key: string]: ITodo[];
 }
 
 export const toDoState = atom<IToDoState>({
   key: 'toDo',
   default: {
-    'To Do': ['a', 'b'],
-    Doing: ['c', 'd', 'e'],
-    Done: ['f'],
-    'Do Later': ['x', 'z'],
+    'To Do': [],
+    Doing: [],
+    Done: [],
+    'Do Later': [],
   },
 });
